fdd801f31d3d5ab4dc9507931af78a41e731acfd
HeoGwangYoung <heogwangyoung@gmail.com>
2023-09-04 09:16:18 +0900

    todo

--- flutter-005-state/lib/main_app_bar.dart
+++ flutter-005-state/lib/main_app_bar.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
 
 AppBar mainAppBar(BuildContext context) => AppBar(
-      title: const Text(
-        "안녕하세요",
-        // style: TextStyle(color: Colors.green),
-      ),
+      title: const Text("안녕하세요"),
       // backgroundColor: Colors.white,
       centerTitle: true,
       elevation: 0.0,
+
       actions: [
         IconButton(
-            onPressed: () {
-              const snackBar = SnackBar(content: Text("안녕하세요"));
-              ScaffoldMessenger.of(context).showSnackBar(snackBar);
-            },
-            icon: const Icon(Icons.person)),
+          onPressed: () {
+            const snackBar = SnackBar(content: Text("안녕하세요"));
+            ScaffoldMessenger.of(context).showSnackBar(snackBar);
+          },
+          icon: const Icon(Icons.person),
+        ),
       ],
     );
