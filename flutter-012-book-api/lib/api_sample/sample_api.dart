72f7ec93bc0dee85fa00cbe9975de9506c69d990
HeoGwangYoung <heogwangyoung@gmail.com>
2023-09-13 15:47:24 +0900

    플러터 네이버 검색 api 기능 활용

---
+++ flutter-012-book-api/lib/api_sample/sample_api.dart
@@ -0,0 +1,55 @@
+import 'dart:convert';
+
+import 'package:book/models/naver_book_dto.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+// ignore_for_file: constant_identifier_names
+const CLIENT_ID = "아이디";
+const CLIENT_SECRET = "시크릿 키";
+const NAVER_BOOK_URL = "https://openapi.naver.com/v1/search/book.json";
+
+class NaverOpenAPI {
+  // Future : JS 의 promise 와 같은 역할
+  // 누군가 loadBooks() 함수를 호출하면
+  // 반드시 데이터를 return 해 줄게 하는 약속
+
+  // 대괄호 매개변수 : 변수를 선택적으로 받겠다
+  Future<List<NaverBookDto>> loadBooks([search]) async {
+    Map<String, String> headers = {
+      "X-Naver-Client-Id": CLIENT_ID,
+      "X-Naver-Client-Secret": CLIENT_SECRET,
+    };
+    String query = search ??= "자바";
+    // ?변수1=값1&변수2=값2&변수3=값3
+    String queryString =
+        Uri(queryParameters: {"query": query, "display": "20", "start": "1"})
+            .query;
+
+    dynamic response;
+
+    try {
+      response = await http.get(
+        Uri.parse("$NAVER_BOOK_URL?$queryString"),
+        headers: headers,
+      );
+    } catch (e) {
+      debugPrint("http 요청 오류");
+    }
+
+    if (response?.statusCode == 200) {
+      debugPrint("응답받은 데이터 : ${response.body.toString()}");
+      Iterable resultItems = await json.decode(response.body)["items"];
+
+      var bookList = resultItems.map((dto) {
+        var result = NaverBookDto.fromJson(dto);
+        return result;
+      });
+      debugPrint(bookList.toString());
+      return bookList.toList();
+    } else {
+      debugPrint("응답 오류 발생 : ${response.body.toString()}");
+      throw Exception("API 연결 문제 발생");
+    }
+  }
+}
