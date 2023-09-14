b83792074717403e301edeeafa4a792b75d80909
HeoGwangYoung <heogwangyoung@gmail.com>
2023-09-14 11:15:32 +0900

    모듈화

--- flutter-012-book-api-v2/lib/main.dart
+++ flutter-012-book-api-v2/lib/main.dart
@@ -1,10 +1,9 @@
-import 'package:book/api/naver_open_api.dart';
-import 'package:book/models/naver_book_dto.dart';
+import './api/naver_open_api.dart';
+import './models/naver_book_dto.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  // NaverOpenAPI().loadBooks();
   runApp(const MainApp());
 }
 
@@ -93,22 +92,69 @@ class ViewListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Future<List<Dto>> type 의 데이터들을 사용하여
+    // 화면에 보여질 Widget을 생성하는 도구
     return SafeArea(
       child: FutureBuilder(
         future: resultBooks,
+        // FutureBuilder 가 resultBooks 데이터를 사용하여 List 데이터로
+        // 변환을 하고, 그 데이터를 다시 ListView.builder 에게 전달하여
+        // 실제로 화면에 보여질 List를 생성한다
         builder: (context, snapshot) {
           return ListView.builder(
             itemCount: snapshot.data!.length,
-            itemBuilder: (context, index) => ListTile(
-              leading: CircleAvatar(
-                backgroundImage: NetworkImage(snapshot.data![index].image!),
-              ),
-              title: Text(snapshot.data![index].title!),
-              subtitle: Text(snapshot.data![index].author!),
+            // ListView.builder 는 리스트의 각 항목(한개 데이터)들을
+            // ListTile 에게 전달하여 한개 데이터를 표현할 View를 만들고
+            // 그들을 모아서 리스트로 보여준다
+            itemBuilder: (context, index) => Padding(
+              padding: const EdgeInsets.all(12),
+              child: BookItemView(bookItem: snapshot.data![index]),
             ),
           );
         },
       ),
+    );
+  }
+}
+
+class BookItemView extends StatelessWidget {
+  const BookItemView({
+    super.key,
+    required this.bookItem,
+  });
+  final NaverBookDto bookItem;
+
+  @override
+  Widget build(BuildContext context) {
+    return ExpansionTile(
+      leading: Image.network(bookItem.image!),
+      title: Text(bookItem.title!),
+      subtitle: Text(bookItem.author!),
+      children: [
+        paddingText(
+          child: Text(
+            bookItem.title!,
+            style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        paddingText(
+          child: Text(bookItem.description!),
+        ),
+        paddingText(
+          child: Text(bookItem.link!),
+        ),
+      ],
+    );
+  }
+
+  // 함수로 분리한 후 return type을 Widget으로 변경
+  Widget paddingText({required Text child}) {
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: child,
     );
   }
 }
